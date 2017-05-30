1bafd1d93f21939f3d7797b6b37c8b96ba66fca9
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-05-29 20:09:31 -0400

    feature/roll-die

--- test/die.js
+++ test/die.js
@@ -1,58 +1,92 @@
 import Die from '../dist/die'
 import 'chai/register-should'
+import MINIMUM_ROLL from '../lib/MINIMUM_ROLL'
+import MINIMUM_SIDES from '../lib/MINIMUM_SIDES'
 
 var die
 var sides
-const minimumSides = 4
 
-describe('die', () => {
-  before(() => {
-    die = new Die()
-  })
+before(() => {
+  die = new Die()
+})
 
+describe('a die', () => {
   it('should have sides', () => {
     die.should.have.property('sides')
   })
 
   describe('sides', () => {
     it('should be a number', () => {
-      die.sides.should.be.a('number')
-    })
-  })
-})
-
-describe('constructing a die', () => {
-  describe('without sides', function() {
-    before(() => {
-      die = new Die()
-    })
-
-    it('should have sides equal to minimum sides', () => {
-      die.sides.should.equal(minimumSides)
+      die.sides.should.be.a('Number')
     })
   })
 
-  describe('with sides', () => {
-    describe('less than the minimum', () => {
-      before(() => {
-        sides = minimumSides - 1
-        die = new Die(sides)
-      })
-
+  describe('constructing a die', () => {
+    describe('without sides', function() {
       it('should have sides equal to minimum sides', () => {
-        die.sides.should.equal(minimumSides)
+        die.sides.should.equal(MINIMUM_SIDES)
       })
     })
 
-    describe('greater than or equal to minimum', () => {
-      before(() => {
-        sides = minimumSides + 1
-        die = new Die(sides)
+    describe('with sides', () => {
+      describe('less than the minimum', () => {
+        before(() => {
+          sides = MINIMUM_SIDES - 1
+          die = new Die(sides)
+        })
+
+        it('should have sides equal to minimum sides', () => {
+          die.sides.should.equal(MINIMUM_SIDES)
+        })
       })
 
-      it('should have size equal specified sides', () => {
-        die.sides.should.equal(sides)
+      describe('greater than or equal to minimum', () => {
+        before(() => {
+          sides = MINIMUM_SIDES + 1
+          die = new Die(sides)
+        })
+
+        it('should have size equal specified sides', () => {
+          die.sides.should.equal(sides)
+        })
       })
     })
+  })
+
+  describe('rolling a die', () => {
+    var dieRolls
+    var possibleDieRolls
+    const NUMBER_OF_DIE_ROLLS = 1000
+    before(() => {
+      sides = 20
+      die = new Die(20)
+      dieRolls = new Array()
+      for (
+        let currentDieRoll = 0;
+        currentDieRoll < NUMBER_OF_DIE_ROLLS;
+        currentDieRoll++
+      ) {
+        dieRolls.push(die.roll())
+      }
+      possibleDieRolls = Array.from(new Set(possibleDieRolls))
+    })
+
+    it('should return a number', () => {
+      die.roll().should.be.a('Number')
+    })
+
+    it('should return a number greater than or equal to 1', () => {
+      possibleDieRolls.every(possibleDieRoll => {
+        return possibleDieRoll >= 1
+      }).should.be.true
+    })
+
+    it('should return a number less than or equal to its sides', () => {
+      possibleDieRolls.every(possibleDieRoll => {
+        return possibleDieRoll <= sides
+      }).should.be.true
+    })
+
+    it('should return a random number', () => {})
   })
 })
