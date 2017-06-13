c69aa0f321b5a9a4e23ee3b435778e4cc411dfdb
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-06-12 20:14:43 -0400

    Add support for node modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import babel from 'rollup-plugin-babel'
 import babelrc from 'babelrc-rollup'
 import istanbul from 'rollup-plugin-istanbul'
+import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
 let pkg = require('./package.json')
 let external = Object.keys(pkg.dependencies)
 
-let plugins = [commonjs(), babel(babelrc())]
+let plugins = [
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs(),
+  babel(babelrc())
+]
 
 if (process.env.BUILD !== 'production') {
   plugins.push(
